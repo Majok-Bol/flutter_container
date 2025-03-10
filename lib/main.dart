2c0365ab82af9f1d5b5528d7b10cb4afd2466906
Majok-Bol <bolmajok001@gmail.com>
2025-03-10 16:12:01 +0300

    added title and body

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: Text('hello world')));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'A simple flutter app',
+            style: TextStyle(fontSize: 25, color: Colors.redAccent),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+
+          decoration: BoxDecoration(
+            color: Colors.white70,
+            border: Border.all(color: Colors.black, width: 2.0),
+
+            borderRadius: BorderRadius.circular(10),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            'Welcome to Flutter framework',
+            style: TextStyle(color: Colors.black, fontSize: 20),
+            // textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
   }
 }
